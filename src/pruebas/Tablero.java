40c562e8fb79ef8c2c60fce6ae530ca8f119f154
Rebeca <rebeca.molinaprados@alu.uclm.es>
2017-10-07 18:51:02 +0200

    .

--- src/pruebas/Tablero.java
+++ src/pruebas/Tablero.java
@@ -12,10 +12,7 @@ public class Tablero {
 	int V; // cantidad de Arena que va haber en todo el tablero
 	int K; // cantidad de arena que va a haber en cada casilla
 	int max; // cantidad máxima que almacena cada casilla
-
-	public Tablero(){
-
-	}
+	
 
 	Tablero(int filas, int columnas) {
 
@@ -33,50 +30,35 @@ public class Tablero {
 		}
 		int [] datos = new int [6];
 
+		int i=0;
 		for (int j = 0; j < 5; j++) {
 			datos[j]=entrada.nextInt();
 		}
-		this.filas=datos[0];
-		this.columnas=datos[1];
-		System.out.println(datos[0]+" colum  "+datos[1]);
-		tablero=new int[this.filas][this.columnas];
+		int [][] terreno=new int [datos[0]][datos[1]];
 		for (int j = 0; j < datos[0]; j++) { //fila
 			for (int k = 0; k < datos[1]; k++) { //columna
-				tablero[j][k]=entrada.nextInt();
+				terreno[j][k]=entrada.nextInt();
 			}
 		}
 	}
-	//*****************************************************
-	// Métodos para generar el Terreno de Manera Aleatoria
-	//*****************************************************
-	
-	void generarTerrenoRnd(){
-		V =(int) (Math.random()*99)+1;;
-		filas = (int) (Math.random()*9)+1;
-		columnas = (int) (Math.random()*9)+1;
-		K=V/filas*columnas;
-		max=(int) (Math.random()*(V-1))+1;; //cantidad máxima que puede almacenar cada casilla, se pasa por parámetros
-		int total=V;
-		tablero=new int[filas][columnas];
+		/*
+		 * Método para rellenar el tablero con números aleatorios
+		 */
+	void rellenarTableroRan(int filas, int columnas) {
 		for (int i = 0; i < filas; i++) {
 			for (int j = 0; j < columnas; j++) {
-				if(total<max){
-					tablero[i][j]=(int) (Math.random()*total);
-				}else{
-					tablero[i][j] = (int) (Math.random()*max);
-				}
-
-				total = total - tablero[i][j];
+				tablero[i][j] = (int) (Math.random() * 9);
 			}
 			System.out.println("\n");
 		}
-
 	}
 
-
-	void imprimirTablero() {
-		for (int i = 0; i < this.filas; i++) {
-			for (int j = 0; j < this.columnas; j++) {
+		/*
+		 * Método para imprimir el tablero generado de manera aleatoria
+		 */
+	void imprimeTablero(int filas, int columnas) {
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
 				System.out.print(tablero[i][j] + "\t");
 			}
 			System.out.println("\n");
