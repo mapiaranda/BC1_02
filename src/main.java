64386568ef0dcdb317b2a13d43f30899b693562e
mapiaranda <MPilar.Aranda@alu.uclm.es>
2017-10-09 17:10:28 +0200

    Todo

--- src/main.java
+++ src/main.java
@@ -1,19 +1,32 @@
 package pruebas;
 
+import java.util.Scanner;
+
 public class main {
 	
 	 public static void main(String[] args) 
 	    {
-		 int filas = (int) (Math.random()*9);
-		 int columnas = (int) (Math.random()*9);
+			Tablero tablero = new Tablero();
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Pulsa 1 para leer de fichero");
+			System.out.println("Pulsa 2 para hacerlo aleatoriamente");
+
+			switch (sc.nextInt()){
+				case 1:
+					tablero.rellenarTableroFichero();
+					tablero.imprimirTablero();
+					break;
+				case 2:
+					tablero.generarTerrenoRnd();
+					tablero.imprimirTablero();
+					break;
+				default:
+					System.out.println("Número incorrecto");
+			}
 
 	     //   tablero = new int[filas][columnas];
 	        
-	        Tablero tablero = new Tablero(filas,columnas);
-	        
-	        tablero.rellenarTableroRan(filas, columnas);
 
-	        tablero.imprimirTablero(filas, columnas);
 	       
 	      
 	}
