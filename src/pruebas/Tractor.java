9d645f3da26df270f113b0a4935f7018f527cf1b
Rebeca <rebeca.molinaprados@alu.uclm.es>
2017-10-10 19:57:13 +0200

    Merge remote-tracking branch 'remotes/origin/mapi'
    # Conflicts:
    #	src/Tablero.java
    #	src/Tractor.java
    #	src/main.java

--- src/pruebas/Tractor.java
+++ src/pruebas/Tractor.java
@@ -31,8 +31,9 @@ public class Tractor {
 	}
 	public void adyacentes(){
 		ArrayList<Casilla> adya=new ArrayList<>();
+		//comprobamos si existe casilla a la derecha, izquierda, arriba y abajo
 		if((posX+1)<Tablero.filas ){
-			if(!Tablero.tablero[posX+1][posY].isVisitado() && !Tablero.tablero[posX][posY].isVisitado()){
+			if(!Tablero.tablero[posX+1][posY].isVisitado() && !Tablero.tablero[posX][posY].isVisitado()){ //Si no esta visitado el adyacente ni estamos en uno que ya hubieramos visitado anteriormente, lo contamos como sucesor
 				contadorSucesores+=1;
 			}
 			Casilla ca=Tablero.tablero[posX+1][posY];
@@ -63,17 +64,17 @@ public class Tractor {
 		Tablero.tablero[posX][posY].setVisitado(true);
 		int contador=0;
 		for (int i=0;i<Tablero.filas;i++){
-			for (int j=0; j<Tablero.columnas;j++){
+			for (int j=0; j<Tablero.columnas;j++){ //Contamos los visitados de todo el tablero
 				if(Tablero.tablero[i][j].isVisitado()){
 					contador+=1;
 				}
 			}
 		}
-		if(contador==(Tablero.filas*Tablero.columnas)){
+		if(contador==(Tablero.filas*Tablero.columnas)){ //Si todos estan visitados ya hemos terminado
 			//Termina
 			System.out.println("El número de sucesores es: "+contadorSucesores);
 		}else{
-				int mover = (int) (Math.random() * (adya.size()));
+				int mover = (int) (Math.random() * (adya.size()));//Generamos un numero aleatorio y nos movemos a el
 				switch (mover) {
 					case 0:
 							setPosX(adya.get(0).getPos_x());
@@ -96,7 +97,7 @@ public class Tractor {
 
 				}
 
-			adyacentes();
+			adyacentes();//Volvemos a llamar al metodo, habiendo actualizado la nueva posicion del tractor
 		}
 	}
 }
