5cbb861f31dc9848ce34beaed1de78b9938b2294
Rebeca <rebeca.molinaprados@alu.uclm.es>
2017-11-06 16:50:51 +0100

    Merge remote-tracking branch 'remotes/origin/mapi'
    # Conflicts:
    #	src/Tablero.java
    #	src/Tractor.java
    #	src/main.java

---
+++ src/pruebas/NodoArbol.java
@@ -0,0 +1,137 @@
+package pruebas;
+
+public class NodoArbol implements Comparable<NodoArbol>{
+	private NodoArbol padre; //información del nodo padre
+	private Terreno Terreno; //representación del terreno actual
+	private int costo; //número de movimientos
+	private String accion; //arribar, abajo, derecha, izquierda
+	private int valor; //valor por el cual se ordenará la frontera
+	private int profundidad; //valor de la profundidad
+	
+	public NodoArbol(NodoArbol padre, Terreno Terreno, int costo, String accion, int valor, int profundidad){
+		this.padre = padre;
+		this.Terreno = Terreno;
+		this.costo = costo;
+		this.accion = accion;
+		this.valor = valor;
+		this.profundidad = profundidad;
+	}
+	
+	/**
+	 * 
+	 * @return NodoArbol
+	 */
+	public NodoArbol getPadre() {
+		return padre;
+	}
+	
+	/**
+	 * 
+	 * @param padre
+	 */
+	public void setPadre(NodoArbol padre) {
+		this.padre = padre;
+	}
+	
+	/**
+	 * 
+	 * @return Terreno
+	 */
+	public Terreno getTerreno() {
+		return Terreno;
+	}
+	
+	/**
+	 * 
+	 * @param Terreno
+	 */
+	public void setTerreno(Terreno Terreno) {
+		this.Terreno = Terreno;
+	}
+
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getCosto() {
+		return costo;
+	}
+	
+	/**
+	 * 
+	 * @param costo
+	 */
+	public void setCosto(int costo) {
+		this.costo = costo;
+	}
+	
+	/**
+	 * 
+	 * @return String
+	 */
+	public String getAccion() {
+		return accion;
+	}
+	
+	/**
+	 * 
+	 * @param accion
+	 */
+	public void setAccion(String accion) {
+		this.accion = accion;
+	}
+	
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getValor() {
+		return valor;
+	}
+	
+	/**
+	 * 
+	 * @param valor
+	 */
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	/**
+	 * 
+	 * @return int
+	 */
+	public int getProfundidad() {
+		return profundidad;
+	}
+	
+	/**
+	 * 
+	 * @param profundidad
+	 */
+	public void setProfundidad(int profundidad) {
+		this.profundidad = profundidad;
+	}
+
+	/**
+	 * Compara el valor de los nodos para la introducción ordenada en la cola con prioridad
+	 * @param nodo
+	 * @return int
+	 */
+	public int compareTo(NodoArbol nodo){
+        int r = 0;
+        if (nodo.getValor() < getValor())
+            r = 1;
+        else if(nodo.getValor() > getValor())
+        	r = -1;
+        
+        return r;
+    }
+	
+	@Override
+	public String toString() {
+		return "NodoArbol [Padre=" + getPadre() + ", Terreno=" + getTerreno() + ", Costo=" + getCosto() 
+		+ ", Accion=" + getAccion() + ", Profundidad=" + getProfundidad() + ", Valor=" + getValor() + "]";
+	}
+	
+}
